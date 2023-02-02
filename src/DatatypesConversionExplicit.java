3fcb64ac4ee92d34e41b5ed28e1785878a241ed9
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:13:04 -0800

    Initial commit

---
+++ src/DatatypesConversionExplicit.java
@@ -0,0 +1,12 @@
+public class DatatypesConversionExplicit {
+    public static void main(String[] args) {
+        double g = 250.56;
+        System.out.println("double representation: "+g);
+        float f =(float)g;
+        System.out.println("float representation: "+f);
+        long h =(long)g;
+        System.out.println("long representation: "+h);
+        int i =(int)h;
+        System.out.println("int representation: "+i);
+    }
+}
